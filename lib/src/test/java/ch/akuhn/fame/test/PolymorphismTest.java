f9e05e7253302f645c3112d1723cd18c9ed21ddc
Benoit Verhaeghe <benoit@badetitou.fr>
2024-10-23 18:40:06 +0200

    remove another JExample dependency

--- lib/src/test/java/ch/akuhn/fame/test/PolymorphismTest.java
+++ lib/src/test/java/ch/akuhn/fame/test/PolymorphismTest.java
@@ -3,42 +3,30 @@ package ch.akuhn.fame.test;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 import ch.akuhn.fame.FameDescription;
 import ch.akuhn.fame.FameProperty;
 import ch.akuhn.fame.Tower;
-import ch.unibe.jexample.Given;
-import ch.unibe.jexample.Injection;
-import ch.unibe.jexample.InjectionPolicy;
-import ch.unibe.jexample.JExample;
 
-@RunWith( JExample.class)
-@Injection(InjectionPolicy.NONE)
 public class PolymorphismTest {
 
     @FameDescription
-    static class A  {
+    static class A {
 
         @FameProperty
         public Object p;
 
-    }    
+    }
 
     static class B extends A {
-        
+
     }
-    
+
+
     @Test
-    public Tower tower() {
+    public void addPolymorphElement() {
         Tower t = new Tower();
         t.metamodel.with(A.class);
-        return t;
-    }
-    
-    @Test
-    @Given("#tower")
-    public void addPolymorphElement(Tower t) {
         int metamodelSize = t.metamodel.size();
         assertEquals(0, t.model.size());
         t.model.add(new B());

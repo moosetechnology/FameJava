e2f6b31b6cfc6233d8593e36ecbd618cb7c39cb9
Benoit Verhaeghe <benoit@badetitou.fr>
2024-10-23 19:29:25 +0200

    should be a better syntax

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -47,7 +47,7 @@ publishing {
   repositories {
     maven {
       name = "GitHubPackages"
-      url = "https://maven.pkg.github.com/moosetechnology/FameJava"
+      url = uri("https://maven.pkg.github.com/moosetechnology/FameJava")
       credentials {
         username = System.getenv("GITHUB_ACTOR")
         password = System.getenv("GITHUB_TOKEN")

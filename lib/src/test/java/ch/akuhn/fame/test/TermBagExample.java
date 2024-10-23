bb811d4ca464e6b7e0f7d15ca27e9d340666dec4
Benoit Verhaeghe <benoit@badetitou.fr>
2024-10-23 18:38:59 +0200

    remove useless import

--- lib/src/test/java/ch/akuhn/fame/test/TermBagExample.java
+++ lib/src/test/java/ch/akuhn/fame/test/TermBagExample.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 import ch.akuhn.fame.FameDescription;
 import ch.akuhn.fame.FamePackage;

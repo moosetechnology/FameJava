798ea6a3aac0f1ab1fa27481398ad51bdcb53b2b
anquetil <nicolas.anquetil@inria.fr>
2024-12-12 18:33:48 +0100

    modified printing of properties to avoid extra "," for entity with no property

--- lib/src/test/java/ch/akuhn/fame/test/JSONPrinterTest.java
+++ lib/src/test/java/ch/akuhn/fame/test/JSONPrinterTest.java
@@ -1,7 +1,9 @@
 package ch.akuhn.fame.test;
 
+import ch.akuhn.fame.MetaRepository;
 import ch.akuhn.fame.Tower;
 import ch.akuhn.fame.internal.JSONPrettyPrinter;
+import ch.akuhn.fame.internal.JSONPrinter;
 import ch.akuhn.fame.parser.InputSource;
 import junit.framework.TestCase;
 
@@ -10,6 +12,9 @@ public class JSONPrinterTest extends TestCase {
     private JSONPrettyPrinter printer;
     private Appendable stream;
 
+    /** FIXME
+     * This is not a test but a method to export a meta-model
+     */
     public void testExportJSON() {
         InputSource input = InputSource.fromResource("ch/unibe/fame/resources/lib.mse");
         Tower t = new Tower();
@@ -28,46 +33,83 @@ public class JSONPrinterTest extends TestCase {
 
     public void testBeginAttributeSimple() {
         printer.beginAttribute("hello");
-        assertEquals(stream.toString(), "\"hello\":");
+        assertEquals("\"hello\":", stream.toString());
     }
 
     public void testPrimitive() {
         printer.primitive("value");
-        assertEquals(stream.toString(), "\"value\"");
+        assertEquals("\"value\"", stream.toString());
     }
 
     public void testPrimitiveWithSpecialCharacter() {
         printer.primitive("MySuper\"String");
-        assertEquals(stream.toString(), "\"MySuper\\\"String\"");
+        assertEquals("\"MySuper\\\"String\"", stream.toString());
     }
 
     public void testPrimitiveWithSpecialCharacterAndActualExample() {
         printer.primitive("print(\"Printer \" + name() + \" prints \"+ thePacket.contents(),false)");
-        assertEquals(stream.toString(), "\"print(\\\"Printer \\\" + name() + \\\" prints \\\"+ thePacket.contents(),false)\"");
+        assertEquals( "\"print(\\\"Printer \\\" + name() + \\\" prints \\\"+ thePacket.contents(),false)\"", stream.toString());
     }
 
     public void testReference() {
         printer.reference("hello");
-        assertEquals(removeWhiteSpaces(stream.toString()), "{\"ref\":\"hello\"}");
+        assertEquals("{\"ref\":\"hello\"}", removeWhiteSpaces(stream.toString()));
     }
 
     public void testReferenceIndex() {
         printer.reference(2);
-        assertEquals(removeWhiteSpaces(stream.toString()), "{\"ref\":2}");
+        assertEquals("{\"ref\":2}", removeWhiteSpaces(stream.toString()));
     }
 
     public void testSerial() {
         printer.serial(2);
-        assertEquals(removeWhiteSpaces(stream.toString()), "\"id\":2,");
+        assertEquals(",\"id\":2", removeWhiteSpaces(stream.toString()));
     }
 
     public void testBeginElement() {
         printer.beginElement("Java.Class");
-        assertEquals(removeWhiteSpaces(stream.toString()), "{\"FM3\":\"Java.Class\",");
+        assertEquals("{\"FM3\":\"Java.Class\"", removeWhiteSpaces(stream.toString()));
     }
 
 
     private static String removeWhiteSpaces(String input) {
         return input.replaceAll("\\s+", "");
     }
+
+    public void testEmptyEntityPrettyPrinter() {
+        String str = "((FM3.Package))";
+        Tower t = new Tower();
+        t.getMetamodel().importMSE(str);
+        MetaRepository repo = t.getMetamodel();
+        repo.accept( printer);
+        assertEquals("[{\"FM3\":\"FM3.Package\",\"id\":1}]", removeWhiteSpaces(stream.toString()));
+    }
+
+    public void testEmptyEntityJSONPrinter() {
+        String str = "((FM3.Package))";
+        Tower t = new Tower();
+        t.getMetamodel().importMSE(str);
+        MetaRepository repo = t.getMetamodel();
+        repo.accept( new JSONPrinter(stream));
+        assertEquals("[{\"FM3\":\"FM3.Package\",\"id\":1}]", stream.toString());
+    }
+
+    public void testEntityWithAttributePrettyPrinter() {
+        String str = "((FM3.Package (name 'Blah')))";
+        Tower t = new Tower();
+        t.getMetamodel().importMSE(str);
+        MetaRepository repo = t.getMetamodel();
+        repo.accept( printer);
+        assertEquals("[{\"FM3\":\"FM3.Package\",\"id\":1,\"name\":\"Blah\"}]", removeWhiteSpaces(stream.toString()));
+    }
+
+    public void testEntityWithAttributeJSONPrinter() {
+        String str = "((FM3.Package (name 'Blah')))";
+        Tower t = new Tower();
+        t.getMetamodel().importMSE(str);
+        MetaRepository repo = t.getMetamodel();
+        repo.accept( new JSONPrinter(stream));
+        assertEquals("[{\"FM3\":\"FM3.Package\",\"id\":1,\"name\":\"Blah\"}]", stream.toString());
+    }
+
 }
